a749fadd21cc266f5800e0841d3d1305d53125ff
felon91 <felon_91@mail.ru>
2020-02-26 20:44:44 +0300

    feat: implement function reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    return (n < 0)
+        ? Number(String(n).slice(1).split('').reverse().join(''))
+        : Number(String(n).split('').reverse().join(''));
 }
